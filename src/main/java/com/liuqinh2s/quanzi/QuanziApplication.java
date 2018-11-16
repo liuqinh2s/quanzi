08638c03036cc9b5ab29f086847302f5375dba9d
liuqinh2s <liuqinh2s@gmail.com>
2018-11-16 20:24:59 +0800

    创建项目

---
+++ src/main/java/com/liuqinh2s/quanzi/QuanziApplication.java
@@ -0,0 +1,12 @@
+package com.liuqinh2s.quanzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanziApplication.class, args);
+    }
+}
---
+++ src/test/java/com/liuqinh2s/quanzi/QuanziApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liuqinh2s.quanzi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuanziApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
